a49333a0f4084d923f62406ff6053b0596b7f2c1
adisney <adisney2@gmail.com>
2014-12-03 07:53:57 -0600

    updated game entry to include new fields. Added test route to trigger email id recording for that game

--- bin/db_ops.sh
+++ bin/db_ops.sh
@@ -12,18 +12,12 @@ function emptySeats(){
   aws dynamodb update-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"56088e90-f894-4282-a244-b2eebd7fdaea\"}}" --attribute-updates "{\"seats\": {\"Value\": {\"L\": []}, \"Action\": \"PUT\"}}" 
 }
 
-function clear(){
-  aws dynamodb delete-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"56088e90-f894-4282-a244-b2eebd7fdaea\"}}"
-  aws dynamodb delete-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"61f1a0ca-af9d-4bb0-a7aa-f450fd61618e\"}}"
-  aws dynamodb delete-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"8b91a3cc-8669-44ae-bf40-0dad6e11d373\"}}"
+function update_gm_email(){
+  aws dynamodb update-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"460acdd5-3aea-4c2d-ac44-ff1b96b89e40\"}}" --attribute-updates "{\"gm_email\": {\"Value\": {\"S\": \"chapman.jeffrey@gmail.com\"}, \"Action\": \"PUT\"}}" 
 }
 
-function update_email(){
-  aws dynamodb update-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"56088e90-f894-4282-a244-b2eebd7fdaea\"}}" --attribute-updates "{\"gm_email\": {\"Value\": {\"S\": \"alexdisney@gmail.com\"}, \"Action\": \"PUT\"}}" 
-}
-
-function update(){
-  aws dynamodb update-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"56088e90-f894-4282-a244-b2eebd7fdaea\"}}" --attribute-updates "{\"email_ids\": {\"Value\": {\"L\": []}, \"Action\": \"PUT\"}}" 
+function update_email_ids(){
+  aws dynamodb update-item --table-name wcpfs-games-test --key "{\"gameId\": {\"S\": \"460acdd5-3aea-4c2d-ac44-ff1b96b89e40\"}}" --attribute-updates "{\"email_ids\": {\"Value\": {\"L\": []}, \"Action\": \"PUT\"}}" 
 }
 
 function deleteSeat(){
